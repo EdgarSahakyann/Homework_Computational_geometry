a1d6cd780f492a6e9cc8e8cd8d4e3a049e8fb68a
EdgarSahakyann <edgar061204@gmail.com>
2025-10-29 21:50:02 +0400

    Create Segment.cpp

---
+++ Segment.cpp
@@ -0,0 +1,21 @@
+#include "Segment.hpp"
+#include <iostream>
+
+Segment::Segment(const Point& a, const Point& b) : m_a(a), m_b(b) {}
+
+bool Segment::onSegment(const Point& p) const {
+    Vector ab(m_a, m_b);
+    Vector ap(m_a, p);
+    if (std::abs(ab.cross(ap)) > 1e-5) return false;
+    float dot = ab.dot(ap);
+    float len2 = ab.dot(ab);
+    return dot >= 0 && dot <= len2;
+}
+
+void Segment::show() const {
+    std::cout << "Segment: ";
+    m_a.show();
+    std::cout << " — ";
+    m_b.show();
+    std::cout << "\n";
+}
