d55b145e8065816b29c2be041dadccb0081c66d9
EdgarSahakyann <edgar061204@gmail.com>
2025-10-29 21:46:32 +0400

    Create Vector.h

---
+++ Vector.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Point.hpp"
+#include <cmath>
+
+class Vector {
+public:
+    Vector(float x = 0, float y = 0);
+    Vector(const Point& from, const Point& to);
+
+    float getX() const;
+    float getY() const;
+    float length() const;
+    void normalize();
+    void show() const;
+
+    Vector operator+(const Vector& other) const;
+    Vector operator-(const Vector& other) const;
+    Vector operator*(float scalar) const;
+    float dot(const Vector& other) const;
+    float cross(const Vector& other) const;
+
+private:
+    float m_x;
+    float m_y;
+};
